29a40995bc37849f651847336946949b8cea46cd
praveen <praveengtm31@gmail.com>
2026-07-26 00:21:38 +0545

    CC-10 feat(search): add fee range filter to college query

--- src/search/filters.js
+++ src/search/filters.js
@@ -2,7 +2,7 @@ const colleges = require('../../data/colleges.json');
 
 function filterColleges(query) {
   let results = colleges;
-  results = results.filter(c => c.course === query.course);
+  results = results.filter(c => c.course === query.course && c.fees <= query.maxFee);
   return results;
 }
 
